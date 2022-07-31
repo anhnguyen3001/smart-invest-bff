74d8ba748e103f3339013ff194b85555a4f7365f
Anh Nguyễn Hoàng <anh.nh1@teko.vn>
2022-07-31 20:47:54 +0700

    handle error response

--- src/common/exceptions.ts
+++ src/common/exceptions.ts
@@ -71,39 +71,20 @@ const nestLogger = new Logger('HandleError');
 // Error handler
 @Catch()
 export class GlobalExceptionsFilter implements ExceptionFilter {
-  catch(exception: HttpException, host: ArgumentsHost): Response<unknown> {
+  catch(exception: any, host: ArgumentsHost): Response<unknown> {
     const ctx = host.switchToHttp();
     const response = ctx.getResponse<Response>();
 
-    if (exception instanceof APIException) {
-      nestLogger.warn(exception.toJSON());
-      return response.status(exception.getStatus()).json(exception.toObj());
-    }
+    nestLogger.error(exception?.stack || exception?.message);
 
-    if (exception instanceof HttpException) {
-      nestLogger.error(
-        (exception as any)?.stack || (exception as any)?.message,
-      );
-      return response
-        .status(exception.getStatus())
-        .json(
-          new APIException(
-            ApiCode[500].UNHANDLED_ERROR.code,
-            exception.getStatus(),
-            (exception.getResponse() as any)?.message ||
-              ApiCode[500].UNHANDLED_ERROR.description,
-          ).toObj(),
-        );
-    }
-
-    nestLogger.error((exception as any)?.stack || (exception as any)?.message);
+    const { code, message } = exception.response.data || {};
     return response
       .status(500)
       .json(
         new APIException(
-          ApiCode[500].UNKNOWN_ERROR.code,
+          code || ApiCode[500].UNKNOWN_ERROR.code,
           500,
-          ApiCode[500].UNKNOWN_ERROR.description,
+          message || ApiCode[500].UNKNOWN_ERROR.description,
           JSON.stringify(exception),
         ).toObj(),
       );
